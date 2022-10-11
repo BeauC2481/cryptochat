e2eb117a40de492bb5f8b3a9cb2815a81561593b
BeauC2481 <91040365+BeauC2481@users.noreply.github.com>
2022-10-11 11:54:27 +1100

    Add files via upload

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import Button from './Button';
+import Banner from './Banner';
+import Input from './Input';
+import Modal from './Modal';
+import Navbar from './Navbar';
+import SearchBar from './SearchBar';
+import shortenAddress from './shortenAddress'
+import Footer from './Footer';
+import Loader from './Loader';
+
+export { Button, Banner, Input, Modal, Navbar, SearchBar, shortenAddress, Footer, Loader };
